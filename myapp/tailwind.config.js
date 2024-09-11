c8a15a32d3029098aec32007ac72c0378d84147d
nookez <nookkup47@gmail.com>
2024-09-11 10:23:35 +0100

    create repo

---
+++ myapp/tailwind.config.js
@@ -0,0 +1,17 @@
+/ @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'media',
+  content: [
+    './templates//*.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        angsana : ["Angsana New"],
+        itim : ["Itim"],
+        kanit:["Kanit"]
+      }
+    },
+  },
+  plugins: [],
+}
